beb169d1a6a68ff952b047f971424ad45346d6be
Justin J <132813834+FustinCS@users.noreply.github.com>
2023-08-03 02:31:33 -0700

    Add resize button to change size of grid

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-function createGrid() {
+function createGrid(size) {
     const grid = document.querySelector(".grid-container");
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < size; i++) {
         const rowContainer = document.createElement("div");
         rowContainer.style.cssText = "display: flex; flex: 1;";
         grid.appendChild(rowContainer);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             const div = document.createElement("div");
             div.setAttribute("class", "paint");
             div.style.cssText = "display: flex; flex: 1; transition: all 0.7s ease;";
             rowContainer.appendChild(div);            
         }
     }
+    // every time we create a new grid, we want to reassign the onHover features
+    onHover();
 }
 
 function hovered(e) {
@@ -24,5 +26,22 @@ function onHover() {
     surface.forEach((square) => square.addEventListener('mouseover', hovered));
 }
 
-createGrid();
-onHover();
+function removeAllChildNodes(parent) {
+    while (parent.firstElementChild) {
+        parent.removeChild(parent.lastElementChild);
+    }
+}
+
+function resize() {
+    let newSize = prompt("Please enter the size you would like to change to.");
+    
+    const grid = document.querySelector(".grid-container");
+    removeAllChildNodes(grid);
+    createGrid(newSize);
+
+}
+
+
+createGrid(16);
+const resizeBtn = document.querySelector(".resize-button");
+resizeBtn.addEventListener('click', resize);
